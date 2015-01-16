4d5aa8e6116d22bd601e728c913e2261dac0d44e
karthik selvakumar <karthikselvakumar7@gmail.com>
2015-01-16 23:43:32 +0000

    Made UI Simple

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,6 +1,6 @@
 class DictionariesController < ApplicationController 
 	def today 
-		word_ids = 5.times.map{ Random.rand(Dictionary.count) } 
+		word_ids = 5.times.map{ Random.rand(1..Dictionary.count) } 
 		@dicts = Dictionary.find(word_ids)
 	end
 
--- app/controllers/proverbs_controller.rb
+++ app/controllers/proverbs_controller.rb
@@ -1,6 +1,6 @@
 class ProverbsController < ApplicationController
 	def today 
-		proverb_ids = 5.times.map{ Random.rand(Proverb.count) } 
+		proverb_ids = 5.times.map{ Random.rand(1..Proverb.count) } 
 		@proverbs = Proverb.find(proverb_ids)
 	end
 end
